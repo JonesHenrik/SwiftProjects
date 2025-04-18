836fa8d11145cf02c6d24d208f3d28df4ba1324a
Henrik Jones <henrikjones22@gmail.com>
2025-04-18 10:11:22 -0400

    starting ToDoList

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Henrik Jones on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
