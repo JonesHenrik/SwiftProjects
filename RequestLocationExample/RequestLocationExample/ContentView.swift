31aba62fdca67c5b9688be93e458d36b4e4ccffc
Henrik Jones <henrikjones22@gmail.com>
2025-05-20 11:44:05 -0400

    request location example

---
+++ RequestLocationExample/RequestLocationExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RequestLocationExample
+//
+//  Created by Henrik Jones on 5/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RequestLocationExample/RequestLocationExample/RequestLocationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestLocationExampleApp.swift
+//  RequestLocationExample
+//
+//  Created by Henrik Jones on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RequestLocationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
